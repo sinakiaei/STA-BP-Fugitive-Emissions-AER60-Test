a12e7c10641b2e719f3c71737555fbe19c627f63
Sina <49888093+sinakiaei@users.noreply.github.com>
2022-01-31 09:49:07 -0700

    Update functions
    - Fix minor issues
    - Improve functions

--- bp-aer69-test.py
+++ bp-aer69-test.py
@@ -7,13 +7,13 @@ def things_name_checker(name):
     # 7 numbers (e.g., ABWP1234567), assigned by Petrinex to each facility. 
     
     if not name.isalnum():
-        return print("A proper facility ID contains 4 letters and 7 numbers")
+        return print('A proper facility ID contains 4 letters and 7 numbers.')
     name1 = name[0:4]
     name2 = name[4:]
     if not name1.isalpha():
-        return print("The first four letters must be alphabetical.")
+        return print('The first four letters must be alphabetical.')
     if not name2.isnumeric():
-        return print("The last seven letters must be numerical.")
+        return print('The last seven letters must be numerical.')
     return print(f"{name} as a thing's name has a valid AER facility ID.")
 
 def datastream_checker(name, ds_number):
@@ -22,38 +22,41 @@ def datastream_checker(name, ds_number):
     # 2. Fugitive Emissions Mass Methane (kg)
     # 3. Number of identified sources of fugitive emissions
 
-    Datastream_names = ["Fugitive Emissions Volume (m3)", "Fugitive Emissions Mass Methane (kg)",
-                        "Number of identified sources of fugitive emissions"]
+    Datastream_names = ['Fugitive Emissions Volume (m3)', 'Fugitive Emissions Mass Methane (kg)',
+                        'Number of identified sources of fugitive emissions']
     datastream_list = []
     
     if ds_number < 3:
-        return print(f'{name} facility ID shall have at least three Datastreams.')
-    elif ds_number >=3:
+        return print(f'{name} facility ID shall have three Datastreams. Not less than 3.')
+    elif ds_number > 3:
+        return print(f'{name} facility ID shall have three Datastreams. Not more than 3.')
+    elif ds_number == 3:
         for n in range(ds_number):
             datastream_name = thing_datastreams['value'][n]['name']
-            if not datastream_name in Datastream_names: # Need improvement
-                return print("Not a valid datastream name")
+            if not datastream_name in Datastream_names:
+                return print(f'{datastream_name} is not a valid datastream name.')
     return print(f'{name} facility ID has at least three separate valid Datastreams.')
 
 def volume_datastream_properities_checker(name, ds_number):
     # This function checks if Fugitive Emissions Volume (m3) datastream has a valid properties.
     # Testing observationType and unitOfMeasurement
 
-    Datastream_name_Volume = "Fugitive Emissions Volume (m3)"
+    Datastream_name_Volume = 'Fugitive Emissions Volume (m3)'
+    
     for n in range(ds_number):
             datastream_name = thing_datastreams['value'][n]['name']
             if datastream_name == Datastream_name_Volume:
                 
                 # Check observationType
-                if not thing_datastreams['value'][n]['observationType'] == "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement":
+                if not thing_datastreams['value'][n]['observationType'] == 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement':
                     return print("Fugitive Emissions Volume (m3) datastream Observation type shall be 'OM_Measurement'.")
                 
                 # Check unitOfMeasurement
-                if not thing_datastreams['value'][n]['unitOfMeasurement']['name'] == "m3":
+                if not thing_datastreams['value'][n]['unitOfMeasurement']['name'] == 'm3':
                     return print("Fugitive Emissions Volume (m3) datastream unitOfMeasurement unit shall be 'm3'.")
-                if not thing_datastreams['value'][n]['unitOfMeasurement']['symbol'] == "Cubic Meter":
+                if not thing_datastreams['value'][n]['unitOfMeasurement']['symbol'] == 'Cubic Meter':
                     return print("Fugitive Emissions Volume (m3) datastream unitOfMeasurement symbol shall be 'Cubic Meter'.")
-                if not thing_datastreams['value'][n]['unitOfMeasurement']['definition'] == "http://qudt.org/vocab/unit/M3":
+                if not thing_datastreams['value'][n]['unitOfMeasurement']['definition'] == 'http://qudt.org/vocab/unit/M3':
                     return print("Fugitive Emissions Volume (m3) datastream unitOfMeasurement definition shall be 'http://qudt.org/vocab/unit/M3'.")
             
             return print(f'Fugitive Emissions Volume datastream properties of {name} facility ID is valid as defined.')
@@ -62,21 +65,21 @@ def mass_datastream_properities_checker(name, ds_number):
     # This function checks if Fugitive Emissions Mass Methane (kg) datastream has a valid properties.
     # Testing observationType and unitOfMeasurement
 
-    Datastream_name_Volume = "Fugitive Emissions Mass Methane (kg)"
+    Datastream_name_Volume = 'Fugitive Emissions Mass Methane (kg)'
     for n in range(ds_number):
             datastream_name = thing_datastreams['value'][n]['name']
             if datastream_name == Datastream_name_Volume:
                 
                 # Check observationType
-                if not thing_datastreams['value'][n]['observationType'] == "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement":
+                if not thing_datastreams['value'][n]['observationType'] == 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement':
                     return print("Fugitive Emissions Mass Methane (kg) datastream Observation type shall be 'OM_Measurement'.")
                 
                 # Check unitOfMeasurement
-                if not thing_datastreams['value'][n]['unitOfMeasurement']['name'] == "kg":
+                if not thing_datastreams['value'][n]['unitOfMeasurement']['name'] == 'kg':
                     return print("Fugitive Emissions Mass Methane (kg) datastream unitOfMeasurement unit shall be 'm3'.")
-                if not thing_datastreams['value'][n]['unitOfMeasurement']['symbol'] == "Kilogram":
+                if not thing_datastreams['value'][n]['unitOfMeasurement']['symbol'] == 'Kilogram':
                     return print("Fugitive Emissions Mass Methane (kg) datastream unitOfMeasurement symbol shall be 'Kilogram'.")
-                if not thing_datastreams['value'][n]['unitOfMeasurement']['definition'] == "http://qudt.org/vocab/unit/KiloGM":
+                if not thing_datastreams['value'][n]['unitOfMeasurement']['definition'] == 'http://qudt.org/vocab/unit/KiloGM':
                     return print("Fugitive Emissions Mass Methane (kg) datastream unitOfMeasurement definition shall be 'http://qudt.org/vocab/unit/KiloGM'.")
             
             return print(f'Fugitive Emissions Mass Methane (kg) datastream properties of {name} facility ID is valid as defined.')
@@ -85,24 +88,25 @@ def number_of_fg_datastream_properities_checker(name, ds_number):
     # This function checks if Number of identified sources of fugitive emissions datastream has a valid properties.
     # Testing observationType
 
-    Datastream_name_Volume = "Number of identified sources of fugitive emissions"
+    Datastream_name_Volume = 'Number of identified sources of fugitive emissions'
     for n in range(ds_number):
             datastream_name = thing_datastreams['value'][n]['name']
             if datastream_name == Datastream_name_Volume:
                 
                 # Check observationType
-                if not thing_datastreams['value'][n]['observationType'] == "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation":
+                if not thing_datastreams['value'][n]['observationType'] == 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation':
                     return print("Number of identified sources of fugitive emissions datastream Observation type shall be 'OM_CountObservation'.")
 
             return print(f'Number of identified sources of fugitive emissions datastream properties of {name} facility ID is valid as defined.')
 
-
+# def observe_properties_checker(name,ds_number):
+    # This function checks if observerdProperty  identified sources of fugitive emissions datastream has a valid properties.
+    # Testing observationType
 
 req_things = requests.get('http://imsw.gswlab.ca:8080/FROST-Server/v1.1/Things')
 things = req_things.json()
 
 # Check the Reporting facility (Thing) has a valid facility ID name.
-
 for n in range(len(things['value'])): 
     thing = things['value'][n]
     things_name_checker(thing['name'])
@@ -121,7 +125,6 @@ for n in range(len(things['value'])):
 print()
 
 # Check 3 valid Datastreams of the Reporting facility (Thing) has valid properties.
-
 for n in range(len(things['value'])): 
     thing = things['value'][n]
     thing_id = things['value'][n]['@iot.id']
